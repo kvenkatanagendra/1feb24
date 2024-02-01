ba4630d9ddf91ecd3af86e0913d331faa9ef7040
kvenkatanagendra <154890600+kvenkatanagendra@users.noreply.github.com>
2024-02-01 11:12:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+
+            var score=0;
+            var score1=0;
+           var score2=0;
+ var score3=0;
+            var score4=0;
+           var score5=0;
+
+var score6=0;
+            function Dec()
+            {
+                score=score-1;
+                document.getElementById("score").textContent=score;
+            }
+              function Inc()
+            {
+                score=score+1;
+                document.getElementById("score").textContent=score;
+                
+            }
+    function Dec1()
+            {
+                score1=score1-1;
+                document.getElementById("score1").textContent=score1;
+            }
+              function Inc1()
+            {
+                score1=score1+1;
+                document.getElementById("score1").textContent=score1;
+            }
+ function Dec2()
+            {
+                score2=score2-1;
+                document.getElementById("score2").textContent=score2;
+            }
+              function Inc2()
+            {
+                score2=score2+1;
+                document.getElementById("score2").textContent=score2;
+            }
+function Dec3()
+            {
+                score3=score3-1;
+                document.getElementById("score3").textContent=score3;
+            }
+              function Inc3()
+            {
+                score3=score3+1;
+                document.getElementById("score3").textContent=score3;
+            }
+function Dec4()
+            {
+                score4=score4-1;
+                document.getElementById("score3").textContent=score4;
+            }
+              function Inc4()
+            {
+                score4=score4+1;
+                document.getElementById("score4").textContent=score4;
+            }
+function Dec5()
+            {
+                score5=score5-1;
+                document.getElementById("score5").textContent=score5;
+            }
+              function Inc5()
+            {
+                score5=score5+1;
+                document.getElementById("score5").textContent=score5;
+            }
+function Dec6()
+            {
+                score6=score6-1;
+                document.getElementById("score6").textContent=score6;
+            }
+              function Inc6()
+            {
+                score6=score6+1;
+                document.getElementById("score6").textContent=score6;
+            }
